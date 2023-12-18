b27ff963598b1a4abbe26edfc4ec219737ecd6a7
laura <laura.blazquez.7e7@itb.cat>
2023-12-18 08:08:57 +0100

    Refactoring

--- Program.cs
+++ Program.cs
@@ -1,43 +1,31 @@
 ﻿using System;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace Projects {
+namespace Projects
+{
     public class MyProject
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             int age;
-            int num;
+            int discount;
             string name;
             Console.WriteLine("Introdueix el nom del client: ");
             name = Console.ReadLine();
             Console.WriteLine("Introdueix l'edat del client: ");
             age = Convert.ToInt32(Console.ReadLine());
-            
+
             Console.WriteLine("Introdueix el descompte de l'entrada: ");
-            if (discount == 0)
-            {
-                Console.WriteLine("No s'ha indicat cap descompte");
-            }
-            else if (discount < 0) {
-                Console.WriteLine("El descompte no pot ser negatiu");
-            }
+            discount = Convert.ToInt32(Console.ReadLine());
+            if (discount == 0) Console.WriteLine("No s'ha indicat cap descompte");
+            else if (discount < 0) Console.WriteLine("El descompte no pot ser negatiu");
             PrintCompte(age, name);
         }
-
         static void PrintCompte(int age, string name)
         {
             PrintHeader();
-            Console.WriteLine("nom: " + age);
-            Console.WriteLine("edat: " + name);
-            if (EntradaGratis(age) == 1)
-            {
-                Console.WriteLine("El client té entrada gratis.");
-            }
-            else 
-            {
-                Console.WriteLine("El client no té entrada gratis.");
-            }    
+            Console.WriteLine("Nom: " + name);
+            Console.WriteLine("Edat: " + age);
+            if (Jubilat(age) == true) Console.WriteLine("El client té entrada gratis.");
+            else Console.WriteLine("El client no té entrada gratis.");
         }
         static void PrintHeader()
         {
@@ -46,10 +34,6 @@ namespace Projects {
             Console.WriteLine("*********** Control clients ***************");
             Console.WriteLine("*******************************************");
             Console.WriteLine("*******************************************");
-        }
-        static int EntradaGratis(int edat)
-        {
-            return (Jubilat(edat)) ? 1 : 0;
         }
         static bool Jubilat(int edat)
         {
